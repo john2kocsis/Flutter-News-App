d31da9a9c8213f72a5843d240081c5e513359b61
john2kocsis <47500519+john2kocsis@users.noreply.github.com>
2020-03-01 14:26:58 +0000

    pulgin dep

--- lib/src/api/api_provider.dart
+++ lib/src/api/api_provider.dart
@@ -4,7 +4,7 @@ import 'package:flutter_news_app/src/model/topheadlinesnews/response_top_headlin
 class ApiProvider {
   final Dio _dio = Dio();
   final String _baseUrl =
-      'https://newsapi.org/v2/top-headlines?country=id&apiKey=fc14ae62fff14511a901ad9537987354';
+      'https://newsapi.org/v2/top-headlines?country=hu&apiKey=de48ce07f2f5423ebc99d87ebe47c779';
 
   void printOutError(error, StackTrace stacktrace) {
     print('Exception occured: $error with stacktrace: $stacktrace');
